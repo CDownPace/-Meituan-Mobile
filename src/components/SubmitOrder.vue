e592a3ff3e593dc0c72ccf90a8562c659b5e5964
CDownPace <3131460235@qq.com>
2020-01-10 17:00:55 +0800

    地址部分

--- src/components/SubmitOrder.vue
+++ src/components/SubmitOrder.vue
@@ -1,23 +1,48 @@
 <template>
 <div class="">
 <mt-nav-bar leftArrowStyle="black" title="确认订单"></mt-nav-bar>
+<van-cell-group>
+  <van-cell title="请选择收货地址" icon="location-o" is-link>
+    <template slot="title" v-if="address">
+      <div class="user-info">
+        <span>{{address.realname}}</span>
+        <span>{{address.telephone}}</span>
+      </div>
+      <div class="address-info">
+        {{address.province}}{{address.city}}{{address.county}}{{address.detail}}
+
+      </div>
+    </template>
+  </van-cell>
+</van-cell-group>
 </div>
 </template>
 
 <script type="text/ecmascript-6">
 import MTNavBar from "./Commen/MTNavBar"
+import { Cell, CellGroup } from 'vant';
 export default{
   name:"SubmitOrder",
   data(){
     return {
-      goodsList:[] 
+      goodsList:[] ,
+      address:{
+        realname:"张三",
+        telephone:"18877775645",
+        province:"湖南省",
+        city:"长沙市",
+        county:"岳麓区",
+        detail:"岳麓区109号"
+      }
     }
   },
   mounted(){
     this.goodsList = this.$store.state.cart
   },
   components:{
-    [MTNavBar.name]:MTNavBar
+    [MTNavBar.name]:MTNavBar,
+    [Cell.name]:Cell,
+    [CellGroup.name]:CellGroup
   }
 }
 </script>
